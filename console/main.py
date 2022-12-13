82e6e88a0deda7a238dfa8ebdad371c9f49f45f3
Тимур Сейидов <timursejidov@MacBook-Pro-Timur.local>
2022-12-13 15:57:13 +0500

    added console app

--- console/main.py
+++ console/main.py
@@ -1,3 +1,14 @@
-import os
-from do
+import sys
+from core.base import Ip
+from core import BaseApplication
+
+if __name__ == '__main__':
+    ip_address = input('Enter IP address: ').strip()
+    if not ip_address:
+        print('IP address is empty')
+        sys.exit()
+    app = BaseApplication(env_file='../.env')
+    result = app.locate(Ip(ip_address))
+    print(result)
+
 
